04b5f4a32776adef7e86fb91ade68106d6a8736a
cdpa1234 <cdpa1234@gmail.com>
2025-10-19 23:36:59 -0500

    fix: Corregir script de inicio para despliegue en Render

--- .next/static/chunks/OneDrive_Escritorio_amerasia-main_src_app_layout_tsx_1ab13796._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[next]_internal_font_google_be385945._.css",
-  "static/chunks/396d6_next_dist_de522063._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_16bb8c57._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_e601f1b7._.js",
-  "static/chunks/src_3ccab0cd._.js"
-],
-    source: "dynamic"
-});
